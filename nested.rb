00e305277b4686c91240f2e4e3f872118d313745
remote-torrent-6747 <abbyc@sky.com>
2020-12-28 12:05:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,15 +83,7 @@ def adding_matz
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"}
 
 return programmer_hash
-
 end
-
-
-
-
-
-
-
 
 
 def changing_alan
@@ -112,9 +104,15 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
-
 end
+
+
+
+
+
+
+
+
 
 def adding_to_dennis
 	programmer_hash =
